af0860c7ce20e8fe46afe087169f77e43aa430c5
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-02-03 01:05:52 +0100

    wip

---
+++ src/Sort.java
@@ -0,0 +1,15 @@
+public class Sort {
+    public static void main(String[]  args) {
+        // create a programme using an array that sort a list of integers in descending orders
+        //Descending order is highest to lowest
+        //in other words if the array have values in it 106, 26, 51, 81, 5
+        //ultimately should have an array with 106, 81, 51,26, 5
+        //setup the program in the form the numbers to sort are read in from keyboard
+        //implement the following methods - getIntegers, printArray, sortIntegers
+        //grtInteers returns an arrayof entered integers from keyboard
+        //printArray prints out the content of the array
+        //and sortIntegers should sort the array and return a new array containing the sorted numbers
+        //you will have to figure out how to copythe array element from the passed array into a new
+        //array and sort them and return the new sorted array
+    }
+}
